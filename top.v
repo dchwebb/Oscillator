9b5c798d642ca1ab261a356a0411c3ce303c12a1
Dominic Webb <dwebb@reapit.com>
2020-01-24 11:23:24 +0000

    Cleaned up top module ensuring correct sample rate

--- top.v
+++ top.v
@@ -10,8 +10,8 @@ module top(dac_spi_cs, dac_spi_data, dac_spi_clock, adc_spi_nss, adc_spi_data, a
 	output wire dac_spi_cs;
 	output wire dac_spi_data;
 	output wire dac_spi_clock;
-	//wire dac_sending;
 	reg [23:0] dac_data;
+	reg send;
 	
 	// ADC settings
 	input wire adc_spi_nss;
@@ -20,44 +20,34 @@ module top(dac_spi_cs, dac_spi_data, dac_spi_clock, adc_spi_nss, adc_spi_data, a
 	wire [15:0] adc_data;
 	wire adc_data_received;
 	
-	// Top timing settings
-	reg send;
-	reg [23:0] send_counter = 23'h00;
-	reg [15:0] counter = 16'h00;
-	reg [1:0] state;
+	// Timing settings
+	reg [15:0] sample_pos;					
+	reg [15:0] sample_timer = 1'b0;
+	parameter SAMPLEINTERVAL = 15'd2015;			// Clock frequency / sample rate - eg 88.67Mhz / 44khz = 2015
+	
+	// Sample settings
+	parameter SEND_CHANNEL_A = 8'b00110001;		// Write to DAC Channel A
 
-	parameter SENDINTERVAL = 23'd3624;
-
-
-	parameter state_waiting	= 2'b00;
-	parameter state_send		= 2'b01;
-	parameter state_sending	= 2'b10;
-	parameter state_sent		= 2'b11;
-
+	// Initialise fpga clock
 	wire fpga_clock;
 	OSCH #(.NOM_FREQ("88.67")) rc_oscillator(.STDBY(1'b0), .OSC(fpga_clock), .SEDSTDBY());
 
 	// Initialise ADC SPI input microcontroller
 	ADC_SPI_In adc(.reset(rst), .spi_nss(adc_spi_nss), .spi_clock_in(adc_spi_clock), .spi_data_in(adc_spi_data), .data_out(adc_data), .data_received(adc_data_received));
 
-
 	// initialise DAC SPI (Maxim5134)
 	DAC_SPI_Out dac(.clock_in(fpga_clock), .reset(rst), .data_in(dac_data), .send(send), .spi_cs_out(dac_spi_cs), .spi_clock_out(dac_spi_clock), .spi_data_out(dac_spi_data));
 
-	parameter SEND_CHANNEL_A = 8'b00110001;		// Write to DAC Channel A
 	initial begin
-		state = state_waiting;
-		counter = 1'b1;
-		send_counter = 1'b0;
+		sample_timer = 1'b0;
 		send = 1'b0;
 	end
 
 
 	always @(posedge fpga_clock or posedge rst) begin
 		if (rst) begin
-			state <= state_waiting;
-			counter <= 1'b1;
-			send_counter <= 1'b0;
+			sample_timer = 1'b0;
+			send = 1'b0;
 		end
 		else begin
 			// process incoming ADC data
@@ -65,33 +55,16 @@ module top(dac_spi_cs, dac_spi_data, dac_spi_clock, adc_spi_nss, adc_spi_data, a
 				dac_data <= {SEND_CHANNEL_A, adc_data};
 			end
 			
-			// send DAC data out
-			case (state)
-				state_waiting:
-					begin
-						send_counter <= send_counter + 1'b1;
-						if (send_counter > SENDINTERVAL) begin
-							counter <= counter + 16'd64;
-							state <= state_send;
-						end
-					end
-				state_send:
-					begin
-						//dac_data <= {SEND_CHANNEL_A, counter};
-						send <= 1'b1;
-						state <= state_sent;
-					end
-				state_sent:
-					begin
-						state <= state_waiting;
-						send <= 1'b0;
-						send_counter <= 1'b0;
-					end
-
-			endcase
+			// send DAC data out once sample timer reaches apporpriate count
+			if (sample_timer > SAMPLEINTERVAL) begin
+				sample_timer <= 1'b0;
+				send <= 1'b1;
+			end
+			else begin
+				sample_timer <= sample_timer + 1'b1;
+				send <= 1'b0;
+			end
 		end
 	end
-
-//	assign top_cs = dac_sending;		// NB stops working without this line ????
 
 endmodule
